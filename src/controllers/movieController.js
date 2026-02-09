df3b9a1a0ae16551ffbe3ba2a72375f50645f1eb
mariaeandrade <dudadeandrade13@gmail.com>
2026-02-09 11:35:56 -0300

    ajusta regras de negocio

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -31,31 +31,46 @@ export const create = async (req, res) => {
         const genero = [
             'Ação', 'Drama', 'Comédia', 'Terror', 'Romance', 'Animação', 'Ficção Científica', 'Suspense'
                 ]
-        if (!title) return res.status(400).json({ error: 'O titulo (title) é obrigatório!' });
-        if (!duration) return res.status(400).json({ error: 'A duração (duration) é obrigatória!' });
+        if (!title) return res.status(400).json({ error: 'O titulo é obrigatório!' });
+        if (!duration) return res.status(400).json({ error: 'A duração é obrigatória!' });
         if (!rating) return res.status(400).json ({ error: 'As avaliações são obrigatórias'})
-        if (!genre) return res.status(400).json({ error: 'O gênero (genre) é obrigatório!' });
+        if (!genre) return res.status(400).json({ error: 'O gênero é obrigatório!' });
         if (title.length <= 3) return res.status(400).json({ error: 'O titulo deve ter mais de 3 caracteres' });
         if (!description) return res.status(400).json({ error: 'A descrição é obrigatória' });
         if (description.length <= 10) return res.status(400).json({ error: 'A descrição deve ter mais de 10 caracteres' });
         if (duration < 0) return res.status(400).json({ error: 'A duração do filme não pode ter um valor negativo' });
         if (duration > 300) return res.status(400).json({ error: 'Um filme com duração excedente a 300 minutos não pode ser registrado' });
         if (!genero.includes(genre)) return res.status(400).json({ error: `O genero so pode ser um dos valores: ${genero}` });
-        if (rating <= 0 || rating > 10) return res.status(400).json({ error: 'Deve estar entre 0 e 10' });
-        if (available != true) return res.status(400).json({ error: 'O filme tem que estar disponivel' });
+        if (rating <= 0 || rating > 10) return res.status(400).json({ error: ' A avaliação eve estar entre 0 e 10' });
+        if (available === false) return res.status(400).json({ error: 'O filme tem que estar disponivel' });
+
+        const filmeExistente = await prisma.movie.findFirst({
+            where: {
+                title: {
+                    equals: title,
+                    mode: 'insensitive',
+                },
+            },
+        });
+
+        if (filmeExistente) return res.status(400).json({ error: 'filme duplicado' });
+        
 
         const data = await model.create({
             title,
             description,
             duration: parseInt(duration),
+            rating,
             genre
         });
 
         res.status(201).json({
             message: 'Registro cadastrado com sucesso!',
             data,
-            rating,
         });
+
+
+
     } catch (error) {
         console.error('Erro ao criar:', error);
         res.status(500).json({ error: 'Erro interno no servidor ao salvar o registro.' });
@@ -120,7 +135,7 @@ export const update = async (req, res) => {
 
 export const remove = async (req, res) => {
     try {
-        const { id } = req.params;
+        const { id, rating} = req.params;
 
         if (isNaN(id)) return res.status(400).json({ error: 'ID inválido.' });
 
